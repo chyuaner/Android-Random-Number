8e2c72ba29fa5756b260e5f17a4295f899708539
元兒～ <yuan817@msn.com>
2012-09-23 00:58:08 +0800

    * FileName:	MainActivity.java  * 更新資訊:  * ├─ v2.4 beta 1 -2012.9.22  * │  ├─ 包上ScrollView讓對話框可以捲動  * │  ├─ 加入水平線外觀  * │  └─ 細部關於對話框的排版

--- src/tw/blogspot/yuan817/random/number/MainActivity.java
+++ src/tw/blogspot/yuan817/random/number/MainActivity.java
@@ -6,6 +6,10 @@
  * 作者: 		元兒～
  * Version: 	v2.2
  * 更新資訊:
+ * ├─ v2.4 beta 1 -2012.9.22
+ * │  ├─ 包上ScrollView讓對話框可以捲動
+ * │  ├─ 加入水平線外觀
+ * │  └─ 細部關於對話框的排版
  * ├─ v2.4 Pre-alpha 1 -2012.9.22
  * │  ├─ 修改AboutDialog分開成showAboutDialog副程式
  * │  ├─ 將about_AlertDialog.setMessage改成自行設計的介面
@@ -86,10 +90,13 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.AssetManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.text.method.ScrollingMovementMethod;
 import android.text.util.Linkify;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
@@ -286,36 +293,46 @@ public class MainActivity extends Activity implements OnClickListener {
 	
 	//顯示"關於"資訊的對話框	
 	public void showAboutDialog(){
+		//建立對話方塊AlertDialog
 		AlertDialog about_AlertDialog = new AlertDialog.Builder(this).create();
-		about_AlertDialog.setTitle(R.string.menu_about);
+		about_AlertDialog.setTitle(R.string.menu_about);	//設定AlertDialog標題
 		
 		try {
 			/*PackageManager package_manager = this.getPackageManager();
 			PackageInfo package_info = package_manager.getPackageInfo(this.getPackageName(), 0);*/
 			//建立Layout面板
+			ScrollView about_AlertDialog_scrollView = new ScrollView(this); 
 			LinearLayout about_AlertDialog_content = new LinearLayout(this);
+			about_AlertDialog_content.setOrientation(LinearLayout.VERTICAL);	//設定為直向的layout
+			about_AlertDialog_content.setPadding(
+					getResources().getDimensionPixelSize(R.dimen.padding_large), 
+					getResources().getDimensionPixelSize(R.dimen.padding_small), 
+					getResources().getDimensionPixelSize(R.dimen.padding_large), 
+					getResources().getDimensionPixelSize(R.dimen.padding_small));	//設定layout的邊界大小（左、上、右、下）
+			//宣告"取得套件資訊"的物件
 			PackageInfo package_info = getPackageManager().getPackageInfo(this.getPackageName(), 0);
-			about_AlertDialog_content.setOrientation(LinearLayout.VERTICAL);
-			about_AlertDialog_content.setPadding(getResources().getDimensionPixelSize(R.dimen.padding_large), getResources().getDimensionPixelSize(R.dimen.padding_large), getResources().getDimensionPixelSize(R.dimen.padding_large), getResources().getDimensionPixelSize(R.dimen.padding_large));
 			
 			//前面的關於字串
 			TextView content_textView = new TextView(this);
-			content_textView.setTextAppearance(this, android.R.style.TextAppearance_Medium);
-			content_textView.setAutoLinkMask(Linkify.ALL);
+			content_textView.setTextAppearance(this, android.R.style.TextAppearance_Medium);	//指定文字樣式為中等大小
+			content_textView.setAutoLinkMask(Linkify.ALL);	//設定成會自動加上連結
 			content_textView.setText(
-					getString(R.string.app_name) + "\n"
+					getString(R.string.app_name) + "\n\n"
 					+ getString(R.string.package_name) +"\n"+ package_info.packageName + "\n"
 					+ getString(R.string.version) + package_info.versionName + "\n"
 					+ getString(R.string.author) + getString(R.string.author_content) + "\n"
 					+ getString(R.string.author_website) +"\n"+ getString(R.string.author_website_content)
-					);
+					+ "\n");
 			about_AlertDialog_content.addView(content_textView);
 			
+			//分隔線
+			View hr = new View(this);	//用View模擬出水平線外觀
+			hr.setBackgroundColor(Color.BLACK);	//設定水平線顏色
+			hr.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 2));	//設定水平線的長、寬
+			about_AlertDialog_content.addView(hr);
 			
 			//讀取文字檔
 			//此code是修改自http://androidbiancheng.blogspot.tw/2011/04/assetmanagerassest.html
-			TextView readme_textView = new TextView(this);
-			readme_textView.setTextAppearance(this, android.R.style.TextAppearance_Small);
 			AssetManager assetManager = getAssets();
 			InputStream inputStream = null;
 			String MyStream;
@@ -336,11 +353,16 @@ public class MainActivity extends Activity implements OnClickListener {
 				MyStream = e.toString();
 			}
 			
+			TextView readme_textView = new TextView(this);
+			readme_textView.setTextAppearance(this, android.R.style.TextAppearance_Small);	//指定文字樣式為小等大小
+			readme_textView.setAutoLinkMask(Linkify.ALL);	//設定成會自動加上連結
+			readme_textView.setMovementMethod(ScrollingMovementMethod.getInstance());
 			readme_textView.setText(MyStream);
 			about_AlertDialog_content.addView(readme_textView);
 			
 			//指定這個面板到這個對話框
-			about_AlertDialog.setView(about_AlertDialog_content);
+			about_AlertDialog_scrollView.addView(about_AlertDialog_content);
+			about_AlertDialog.setView(about_AlertDialog_scrollView);
 			
 			/*about_AlertDialog.setMessage(
 					getString(R.string.app_name) + "\n"
